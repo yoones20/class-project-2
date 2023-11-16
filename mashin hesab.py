67271151b8c640d485a0adcf1fb82f63c9ee8f7d
yoones <yoonesakbari3@gmail.com>
2023-11-15 23:38:16 -0800

    import sys & PyQt6

---
+++ mashin hesab.py
@@ -0,0 +1,9 @@
+import sys
+#1. Import `QApplication` and all the required widgets
+from PyQt6.QtWidgets import QApplication
+
+from PyQt6.QtWidgets import QLabel
+
+from PyQt6.QtWidgets import QWidget
+
+app = QApplication(sys.argv)
