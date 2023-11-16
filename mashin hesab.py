165c57824b5991fe095f345bf28415938db8d849
yoones <yoonesakbari3@gmail.com>
2023-11-15 23:41:23 -0800

    add window

--- mashin hesab.py
+++ mashin hesab.py
@@ -7,3 +7,14 @@ from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QWidget
 
 app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
+helloMsg.move(60, 15)
+
+window.show()
+
+sys.exit(app.exec_())
